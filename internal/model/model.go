4a6da8c689772453a864dbc426c369986eac6600
damien venant-valery <damien@localhost.localdomain>
2025-05-23 16:49:45 +0200

    feat: add models

---
+++ internal/model/model.go
@@ -0,0 +1,26 @@
+package model
+
+type (
+	PipelineRuns struct {
+		Id     int    `json:"id"`
+		Name   string `json:"name"`
+		State  string `json:"state"`
+		Result string `json:"result"`
+	}
+
+	BuildChanges struct {
+		Id      string `json:"id"`
+		Message string `json:"message"`
+	}
+
+	BuildWorkItems struct {
+		Id  int    `json:"id"`
+		Url string `json:"url"`
+	}
+
+	OperationFields struct {
+		Op   string `json:"op"`
+		Path string `json:"path"`
+		Value string 'json:"value"'
+	}
+)
