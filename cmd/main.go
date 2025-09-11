42fd8f9984a280ab349c49ec2ff4e3e56cbcebad
Damien Venant-Valéry <damien.venantvalery@septeo.com>
2025-09-11 10:55:14 +0200

    feat: add command

--- cmd/main.go
+++ cmd/main.go
@@ -14,7 +14,7 @@ var (
 	baseUrl      string = ""
 	organisation string = ""
 	project      string = ""
-	version_tool string = "debug_X.X.X"
+	versionTool  string = "debug_X.X.X"
 	pipelineId   int32
 )
 
@@ -25,25 +25,36 @@ var rootCommand = &cobra.Command{
 }
 
 var versionCommand = &cobra.Command{
-	Use:  "version",
-	Long: "Get the version",
-	Run:  funcVersion,
+	Use:   "version",
+	Long:  "Get the version",
+	Short: "Get the version",
+	Run:   funcVersion,
+}
+
+var launchCommand = &cobra.Command{
+	Use:   "start",
+	Short: "Start the prev-updater",
+	Long:  "Start the prev-updater command to changes fields in ADO cards",
+	Run:   funcStart,
 }
 
 func init() {
 	token = os.Getenv("PREV_UPDATER_TOKEN")
 	baseUrl = os.Getenv("PREV_UPDATER_BASEURL")
 
-	rootCommand.Flags().StringVarP(&token, "token", "t", "", "set ADO token (required)")
-	rootCommand.Flags().StringVarP(&baseUrl, "base-url", "b", "https://dev.azure.com/", "set base url")
-	rootCommand.Flags().StringVarP(&organisation, "organisation", "o", "", "set organisation (required)")
-	rootCommand.Flags().Int32VarP(&pipelineId, "pipeline-id", "i", 0, "set pipeline id")
-	rootCommand.Flags().StringVarP(&project, "project", "p", "", "project name")
-	rootCommand.MarkFlagRequired("token")
-	rootCommand.MarkFlagRequired("organisation")
-	rootCommand.MarkFlagRequired("project")
-	rootCommand.MarkFlagRequired("pipeline-id")
+	launchCommand.Flags().StringVarP(&token, "token", "t", "", "set ADO token (required)")
+	launchCommand.Flags().StringVarP(&baseUrl, "base-url", "b", "https://dev.azure.com/", "set base url")
+	launchCommand.Flags().StringVarP(&organisation, "organisation", "o", "", "set organisation (required)")
+	launchCommand.Flags().Int32VarP(&pipelineId, "pipeline-id", "i", 0, "set pipeline id")
+	launchCommand.Flags().StringVarP(&project, "project", "p", "", "project name")
+
+	launchCommand.MarkFlagRequired("token")
+	launchCommand.MarkFlagRequired("organisation")
+	launchCommand.MarkFlagRequired("project")
+	launchCommand.MarkFlagRequired("pipeline-id")
+
 	rootCommand.AddCommand(versionCommand)
+	rootCommand.AddCommand(launchCommand)
 }
 
 func main() {
@@ -52,6 +63,18 @@ func main() {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
+
+}
+
+func funcVersion(cmd *cobra.Command, args []string) {
+	fmt.Println(versionTool)
+}
+
+func funcRun(cmd *cobra.Command, args []string) {
+
+}
+
+func funcStart(cmd *cobra.Command, args []string) {
 	url := fmt.Sprintf("%s/%s/%s/", baseUrl, organisation, project)
 	infra.ConfigureHttpClient(&infra.HttpClientConfiguration{
 		BaseUrl: url,
@@ -59,15 +82,11 @@ func main() {
 	})
 	client := infra.GetHttpClient()
 
-	fmt.Println(url)
 	repo := repository.New(client)
-	repo.GetPipelineRuns(862)
-}
+	lastRun, err := repo.GetPipelineRuns(862)
+	if err != nil {
+		panic(err)
+	}
 
-func funcVersion(cmd *cobra.Command, args []string) {
-	fmt.Println(version_tool)
-}
-
-func funcRun(cmd *cobra.Command, args []string) {
-
+	fmt.Println(lastRun[0].Name)
 }
