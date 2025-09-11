1d355b09ccc14d49f757955f06b546e4e722e23c
Damien Venant-Valéry <damien.venantvalery@septeo.com>
2025-09-11 12:07:18 +0200

    feat: tests

--- internal/usescases/update-fields.go
+++ internal/usescases/update-fields.go
@@ -57,6 +57,7 @@ func (u *AdoUsesCases) UpdateFieldsByPipelineId(pipelineId int) error {
 	return nil
 }
 
+// Test
 func (u *AdoUsesCases) updateFields(woritemId, name string) error {
 	repo := u.Repository
 	modelToUpdload := model.OperationFields{
