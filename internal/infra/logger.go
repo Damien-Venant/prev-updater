95ef882c56f69d0f008bfc004bd25b5732ab3573
Damien-Venant <damien.venantvalery@septeo.com>
2025-09-15 11:03:08 +0200

    feat: add logging to trace some events
    * tests: add unit tests on setHeader functions
    
    * tests: add tests on setHeader functions
    
    * feat: add logging on stderr
    
    * feat: add zerolog package
    
    * feat: add log
    
    * feat: implemnts logs
    
    * feat: add logging on http client
    
    * feat: add some context in http client
    
    * fix: return error code

---
+++ internal/infra/logger.go
@@ -0,0 +1,42 @@
+package infra
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/rs/zerolog"
+)
+
+func NewLogger(writter io.Writer) *zerolog.Logger {
+	output := zerolog.ConsoleWriter{
+		Out:        os.Stdout,
+		NoColor:    true,
+		TimeFormat: time.RFC3339,
+	}
+
+	output.FormatLevel = func(i interface{}) string {
+		return strings.ToUpper(fmt.Sprintf("| %-6s|", i))
+	}
+	output.FormatMessage = func(i interface{}) string {
+		return fmt.Sprintf("***%s****", i)
+	}
+	output.FormatFieldName = func(i interface{}) string {
+		return fmt.Sprintf("%s:", i)
+	}
+	output.FormatFieldValue = func(i interface{}) string {
+		return fmt.Sprintf("%s", i)
+	}
+
+	w := zerolog.MultiLevelWriter(writter, output)
+
+	logger := zerolog.
+		New(w).
+		With().
+		Timestamp().
+		Logger()
+
+	return &logger
+}
--- pkg/http-client/http-client.go
+++ pkg/http-client/http-client.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
+
+	"github.com/rs/zerolog"
 )
 
 type (
@@ -11,6 +13,7 @@ type (
 		BaseUrl string
 		Headers http.Header
 		client  *http.Client
+		logger  *zerolog.Logger
 	}
 )
 
@@ -18,18 +21,28 @@ const (
 	formatUrl string = "%s/%s"
 )
 
-func New(baseUrl string, headers http.Header) *HttpClient {
+func New(baseUrl string, headers http.Header, logger *zerolog.Logger) *HttpClient {
 	return &HttpClient{
 		BaseUrl: baseUrl,
 		Headers: headers,
 		client:  &http.Client{},
+		logger:  logger,
 	}
 }
 
 func (h *HttpClient) Get(path string, headers http.Header) (*http.Response, error) {
 	url := fmt.Sprintf(formatUrl, h.BaseUrl, path)
+	h.logger.
+		Info().
+		Dict("request-data", zerolog.Dict().Str("url", url).Str("method", "GET")).
+		Msg("Send request")
 	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
+		h.logger.
+			Error().
+			Stack().
+			Err(err).
+			Send()
 		return nil, err
 	}
 	request.Header = h.Headers
@@ -40,21 +53,26 @@ func (h *HttpClient) Get(path string, headers http.Header) (*http.Response, erro
 
 func (h *HttpClient) Patch(path string, body []byte, headers http.Header) (*http.Response, error) {
 	url := fmt.Sprintf("%s/%s", h.BaseUrl, path)
+	h.logger.
+		Info().
+		Dict("request-data", zerolog.Dict().Str("url", url).Str("method", "PATCH").Str("body", string(body))).
+		Msg("Send request")
 	request, err := http.NewRequest("PATCH", url, bytes.NewReader(body))
 	if err != nil {
+		h.logger.
+			Error().
+			Stack().
+			Err(err).
+			Send()
 		return nil, err
 	}
 	request.Header = h.Headers
 	request.Header.Set("Content-Type", "application/json-patch+json")
 	setHeader(request, headers)
-	if err != nil {
-		return nil, err
-	}
 
 	return h.client.Do(request)
 }
 
-// TODO : write a test for this function
 func setHeader(request *http.Request, headers http.Header) {
 	for key, values := range headers {
 		for _, val := range values {
